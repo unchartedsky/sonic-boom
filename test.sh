f2503771b7c514ebbb506c85db0b12c94af2545e
Jaehoon Choi <plaintext@andromedarabbit.net>
2025-02-14 20:33:26 +0900

    Support linux/arm64

--- test.sh
+++ test.sh
@@ -3,4 +3,4 @@ set -e
 set -x
 
 # curl --verbose https://reqres.in/api/users?page=2
-curl --verbose http://localhost:8000/ex1/api/users?page=2
+curl --verbose -XGET -H 'Content-Type: application/json' http://localhost:8000/ex1/api/users?page=2
